1d0c52398160d580a12a33051b7205b391791171
Test1calCutter <108223454+Test1calCutter@users.noreply.github.com>
2022-11-20 22:17:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+$('a[href^="#"]').on("click",function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $("html, body").stop().animate({
+	        "scrollTop": $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+
+$(window).scroll(function() {    
+  var scroll = $(window).scrollTop();
+  if (scroll > window.innerHeight - 55) {
+      $(".navbar").removeClass("transparent");
+    } else {
+      $(".navbar").addClass("transparent");
+    }
+});
+
+$("body").scrollspy({target: ".navbar"}) 
+
+$(".thumbnail-overlay").on("click", openProject.bind( this ))
+$(".close-window").on("click", closeWindow.bind(this))
+
+function openProject( event ) {
+  if ( $(event.target).next(".thumbnail-overlay") ) {
+    var id =  "#" + $(event.target).parent().attr("page");
+    $(id).fadeIn()
+  } else {
+    var id =  "#" + $(event.target).attr("page");
+    $(id).fadeIn()
+  }
+}
+
+function closeWindow( event ) {
+  $(event.target).parents(".full-screen").fadeOut();
+}
